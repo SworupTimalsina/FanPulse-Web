2441361f5d97a7f899133e1ee31fd06575cd2532
SworupTimalsina <112379783+SworupTimalsina@users.noreply.github.com>
2025-01-08 09:28:56 +0545

    mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 require('dotenv').config();
 
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log("Mongoose Connection ERROR: " + err.message);
+});
+
+mongoose.connection.once('open', () => {
+    console.log("MongoDB Connected");
+});
+
 const app = require('./app');
 
-app.listen(8000, () => {
-    console.log("Server Listening on port 8000");
+app.listen(3000, () => {
+    console.log("Server Listening on port 3000");
 });
